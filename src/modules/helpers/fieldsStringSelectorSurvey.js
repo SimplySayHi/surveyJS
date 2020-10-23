bec1d0e99609af30a9d7c20bccb33e591f2149f1
Valerio Di Punzio <sayhi@valeriodipunzio.com>
2020-10-23 20:34:44 +0200

    fixed bug for fieldsStringSelectorSurvey for external fields

--- src/modules/helpers/fieldsStringSelectorSurvey.js
+++ src/modules/helpers/fieldsStringSelectorSurvey.js
@@ -1,2 +1,2 @@
 
-export const fieldsStringSelectorSurvey = '[data-surveyjs-form] input:not([type="reset"]):not([type="submit"]):not([type="button"]), [data-surveyjs-form] select, [data-surveyjs-form] textarea, [data-surveyjs-wrapper] [data-surveyjs-external] [data-field]'
+export const fieldsStringSelectorSurvey = '[data-surveyjs-form] input:not([type="reset"]):not([type="submit"]):not([type="button"]), [data-surveyjs-form] select, [data-surveyjs-form] textarea, [data-surveyjs-external] [data-field]'
