d1bdca48c9fddeae51bf407f26729cefacea6a80
Valerio Di Punzio <sayhi@valeriodipunzio.com>
2020-10-28 18:20:44 +0100

    updated lite demo

--- js/demos/demo-basic-lite.js
+++ js/demos/demo-basic-lite.js
@@ -1,4 +1,14 @@
 
 var formEl = document.querySelector('[data-surveyjs-form]');
 var options = { url: '../json/survey-simple.json' };
+
+formEl.addEventListener('fjs.form:submit', function( event ){
+    event.data
+        .then(function(response){
+            // FORM VALIDATION AND SUBMIT ARE UP TO YOU :)
+        })
+        .catch(function(error){})
+        .finally(function(){});
+});
+
 var mySurvey = new Survey( formEl, options );
